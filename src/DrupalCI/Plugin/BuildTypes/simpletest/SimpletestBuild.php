4a9fe7b830f58a083479578800c39c7fa758c73d
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-11-10 13:29:15 -0800

    Issue #2489530: Strips out extraneous loc from these remnants

--- src/DrupalCI/Plugin/BuildTypes/simpletest/SimpletestBuild.php
+++ src/DrupalCI/Plugin/BuildTypes/simpletest/SimpletestBuild.php
@@ -15,90 +15,6 @@ use DrupalCI\Build\Build;
 
 class SimpletestBuild extends Build {
 
-  /**
-   * @var string
-  */
-  public $buildType = 'simpletest';
-
-  /**
-   * Default Arguments (defaultArguments)
-   *
-   * @var array
-   *
-   * Each DrupalCI Build type needs to contain a 'defaultArguments' property,
-   * which contains a list of DCI_* variables and default values; which defines
-   * the default behaviour of that build type if no additional overrides are
-   * passed into an instance of that build type.
-   */
-  public $defaultArguments = array(
-    // ** Environment Defaults
-    'DCI_DBType' => 'mysql',
-    'DCI_DBVersion' => '5.5',
-    // ** Database Default
-    'DCI_DBUser' => 'drupaltestbot',
-    'DCI_DBPassword' => 'drupaltestbotpw',
-    'DCI_DBUrl' => 'dbtype://host', // DBVersion, DBUser and DBPassword variable plugins will change this.
-    // ** startcontainers defaults
-    'DCI_PHPVersion' => '5.5',
-
-
-    // Codebase
-    'DCI_CoreRepository' => 'git://drupalcode.org/project/drupal.git',
-    'DCI_CoreBranch' => '8.0.x',
-    'DCI_GitCheckoutDepth' => '1',
-    'DCI_GitCommitHash' => '',
-
-    // Simpletest Defaults
-    'DCI_RunScript' => '/var/www/html/core/scripts/run-tests.sh ',
-    'DCI_TestGroups' => '--all',
-    'DCI_SQLite' => '/var/www/html/artifacts/simpletest.sqlite',
-    'DCI_Concurrency' => 4,
-    'DCI_RunOptions' => 'color;keep-results',
-    'DCI_PHPInterpreter' => '/opt/phpenv/shims/php',
-
-    // 'DCI_XMLOutput' => '/var/www/html/results/xml',
-    // Environment
-    'DCI_SyntaxCheck' => TRUE,
-  );
-
-  /**
-   * Required Arguments, which must be present in order for the build to attempt
-   * to run.
-   *
-   * The expected format here is an array of key=>value pairs, where the key is
-   * the name of a DCI_* environment variable, and the value is the array key
-   * path from the parsed .yml file build definition that would need to be
-   * traversed to get to the location that variable would exist in the build
-   * definition.
-   *
-   * As an example, DCI_DBVersion defines the database type (mysql, pgsql, etc)
-   * for a given build. In a parsed .yml build definition file, this information
-   * would be stored in the value located at:
-   * array(
-   *   'environment' => array(
-   *     'db' => VALUE
-   *   )
-   * );
-   * Thus, thus the traversal path value stored in the 'requiredArguments'
-   * array is the array keys 'environment:db'.
-   *
-   * As any required arguments for the simpletest build type are defined in the
-   * 'defaultArguments' property, this array is empty.  However, that may not
-   * always be the case for other build types.
-   */
-  public $requiredArguments = array(
-
-  );
-
-  /**
-   * Return an array of possible argument variables for this build type.
-   *
-   * The 'availableArguments' property is intended to provide a complete list
-   * of possible variable values which can affect this particular build type,
-   * along with details regarding how each variable affects the build operation.
-   * These are specified in an array, with the variable names used as the keys
-   * for the array and the description used as the array values.
-   */
   public $_availableArguments = array(
     // ***** Variables Available for any build type *****
     'DCI_UseLocalCodebase' => 'Used to define a local codebase to be cloned (instead of performing a Git checkout)',
@@ -136,22 +52,6 @@ class SimpletestBuild extends Build {
     'DCI_RunScriptArguments' => 'An array of other build script options which will be added to the runScript command when executing a build.',
     // Syntax: 'argkey1,argvalue1;argkey2,argvalue2;argkey3;argkey4,argvalue4;'
     'DCI_GitCommitHash' => 'This allows to checkout specific core commits, useful for regression testing',
-  );
-
-  /**
-   * Identify any priority variables which must be pre-processed before others
-   *
-   * Variables provided in this array will be prioritized and run first, before
-   * any other DCI_* variable preprocessors are executed.
-   */
-  public $priorityArguments = array(
-    // CoreProject doesn't *need* to run first, but it seems like a good idea
-    'DCI_CoreProject',
-    // Expand run options to their argument format, before adding arguments
-    'DCI_RunOptions',
-    // CoreBranch needs to be able to override the SQLite variable before the
-    // SQLite option is added to RunOptions
-    'DCI_CoreBranch',
   );
 
 }
--- src/DrupalCI/Plugin/BuildTypes/simpletestlegacy7/SimpletestLegacy7Build.php
+++ src/DrupalCI/Plugin/BuildTypes/simpletestlegacy7/SimpletestLegacy7Build.php
@@ -15,82 +15,6 @@ use DrupalCI\Build\Build;
 
 class SimpletestLegacy7Build extends Build {
 
-  /**
-   * Build Type (buildType)
-   *
-   * @var string
-   *
-   * This property is not referenced in the current code, but it is anticipated
-   * that others may want to reference the build type from the object itself at
-   * some point in the future.
-   */
-  public $buildType = 'simpletestlegacy7';
-
-  /**
-   * Default Arguments (defaultArguments)
-   *
-   * @var array
-   *
-   * Each DrupalCI Build type needs to contain a 'defaultArguments' property,
-   * which contains a list of DCI_* variables and default values; which defines
-   * the default behaviour of that build type if no additional overrides are
-   * passed into an instance of that build type.
-   */
-  public $defaultArguments = array(
-    'DCI_DBVersion' => 'mysql-5.5',
-    'DCI_PHPVersion' => '5.3',
-    'DCI_CoreRepository' => 'git://drupalcode.org/project/drupal.git',
-    'DCI_CoreBranch' => '7.x',
-    'DCI_GitCheckoutDepth' => '1',
-    'DCI_GitCommitHash' => '',
-    'DCI_RunScript' => '/var/www/html/scripts/run-tests.sh ',
-    'DCI_DBUser' => 'drupaltestbot',
-    'DCI_DBPassword' => 'drupaltestbotpw',
-    'DCI_DBUrl' => 'dbtype://host', // DBVersion, DBUser and DBPassword variable plugins will change this.
-    'DCI_TestGroups' => '--all',
-    'DCI_Concurrency' => 4,
-    'DCI_PHPInterpreter' => '/opt/phpenv/shims/php',
-    'DCI_RunOptions' => 'color',
-  );
-
-  /**
-   * Required Arguments, which must be present in order for the build to attempt
-   * to run.
-   *
-   * The expected format here is an array of key=>value pairs, where the key is
-   * the name of a DCI_* environment variable, and the value is the array key
-   * path from the parsed .yml file build definition that would need to be
-   * traversed to get to the location that variable would exist in the build
-   * definition.
-   *
-   * As an example, DCI_DBVersion defines the database type (mysql, pgsql, etc)
-   * for a given build. In a parsed .yml build definition file, this information
-   * would be stored in the value located at:
-   * array(
-   *   'environment' => array(
-   *     'db' => VALUE
-   *   )
-   * );
-   * Thus, thus the traversal path value stored in the 'requiredArguments'
-   * array is the array keys 'environment:db'.
-   *
-   * As any required arguments for the simpletest build type are defined in the
-   * 'defaultArguments' property, this array is empty.  However, that may not
-   * always be the case for other build types.
-   */
-  public $requiredArguments = array(
-
-  );
-
-  /**
-   * Return an array of possible argument variables for this build type.
-   *
-   * The 'availableArguments' property is intended to provide a complete list
-   * of possible variable values which can affect this particular build type,
-   * along with details regarding how each variable affects the build operation.
-   * These are specified in an array, with the variable names used as the keys
-   * for the array and the description used as the array values.
-   */
   public $availableArguments = array(
     // ***** Variables Available for any build type *****
     'DCI_UseLocalCodebase' => 'Used to define a local codebase to be cloned (instead of performing a Git checkout)',
@@ -127,22 +51,6 @@ class SimpletestLegacy7Build extends Build {
     'DCI_ResultsDirectory' => 'Defines the local directory within the container where the xml results file should be written.',
     'DCI_RunScriptArguments' => 'An array of other build script options which will be added to the runScript command when executing a build.',
     // Syntax: 'argkey1,argvalue1;argkey2,argvalue2;argkey3;argkey4,argvalue4;'
-  );
-
-  /**
-   * Identify any priority variables which must be pre-processed before others
-   *
-   * Variables provided in this array will be prioritized and run first, before
-   * any other DCI_* variable preprocessors are executed.
-   */
-  public $priorityArguments = array(
-    // IsDrupal doesn't *need* to run first, but it seems like a good idea
-    'DCI_IsDrupal',
-    // Expand run options to their argument format, before adding arguments
-    'DCI_RunOptions',
-    // CoreBranch needs to be able to override the SQLite variable before the
-    // SQLite option is added to RunOptions
-    'DCI_CoreBranch',
   );
 
 }
