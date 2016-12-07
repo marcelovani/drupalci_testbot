d5e34ac9e2d85ac30dadb2358ca02786f0547df4
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-12-07 08:42:09 -0800

    Issue #2831615: Checkout no longer mixes replicating a local directory with checking out a repository from git.

--- src/DrupalCI/Plugin/BuildTask/BuildStep/CodebaseAssemble/Checkout.php
+++ src/DrupalCI/Plugin/BuildTask/BuildStep/CodebaseAssemble/Checkout.php
@@ -79,14 +79,7 @@ class Checkout extends BuildTaskBase implements BuildStepInterface, BuildTaskInt
   public function run() {
     $this->io->writeln("<info>Populating container codebase data volume.</info>");
     foreach ($this->configuration['repositories'] as $repository ) {
-      switch ($repository['protocol']) {
-        case 'local':
-          $this->setupCheckoutLocal($repository);
-          break;
-        case 'git':
-          $this->setupCheckoutGit($repository);
-          break;
-      }
+      $this->setupCheckoutGit($repository);
     }
     return;
   }
@@ -96,45 +89,9 @@ class Checkout extends BuildTaskBase implements BuildStepInterface, BuildTaskInt
    */
   public function getDefaultConfiguration() {
     return [
-      'repositories' => [
-        [
-          'protocol' => 'git',
-          'repo' => 'git://drupalcode.org/project/drupal.git',
-          'branch' => '8.0.x',
-          'depth' => '1',
-          'checkout_dir' => '.',
-          'source_dir' => './',
-        ]
-      ],
+      'repositories' => [],
     ];
 
-  }
-
-  protected function setupCheckoutLocal($repository) {
-    $source_dir = isset($repository['source_dir']) ? $repository['source_dir'] : './';
-    $checkout_dir = isset($repository['checkout_dir']) ? $repository['checkout_dir'] : $this->build->getSourceDirectory();
-    // TODO: Ensure we don't end up with double slashes
-    // Validate source directory
-    if (!is_dir($source_dir)) {
-      $this->io->drupalCIError("Directory error", "The source directory <info>$source_dir</info> does not exist.");
-      throw new BuildTaskException("The source directory $source_dir does not exist.");
-    }
-    // Validate target directory.  Must be within workingdir.
-    if (!($directory = $this->validateDirectory($this->build->getSourceDirectory(), $checkout_dir))) {
-      // Invalidate checkout directory
-      $this->io->drupalCIError("Directory error", "The checkout directory <info>$directory</info> is invalid.");
-      throw new BuildTaskException("The checkout directory $directory is invalid.");
-    }
-    $this->io->writeln("<comment>Copying files from <options=bold>$source_dir</options=bold> to the local checkout directory <options=bold>$directory</options=bold> ... </comment>");
-    // TODO: Make sure target directory is empty
-    #  $this->exec("cp -r $source_dir/. $directory", $cmdoutput, $result);
-    $exclude_var = isset($repository['DCI_EXCLUDE']) ? '--exclude="' . $repository['DCI_EXCLUDE'] . '"' : "";
-    $this->exec("rsync -a $exclude_var  $source_dir/. $directory", $cmdoutput, $result);
-    if ($result !== 0) {
-      $this->io->drupalCIError("Copy error", "Error encountered while attempting to copy code to the local checkout directory.");
-      throw new BuildTaskException("The rsync returned an error.  Error Code: $result");
-    }
-    $this->io->writeln("<comment>DONE</comment>");
   }
 
   protected function setupCheckoutGit($repository) {
@@ -142,7 +99,7 @@ class Checkout extends BuildTaskBase implements BuildStepInterface, BuildTaskInt
     // @TODO: these should always have a default. no sense in setting them here.
     $repo = isset($repository['repo']) ? $repository['repo'] : 'git://drupalcode.org/project/drupal.git';
 
-    $git_branch = isset($repository['branch']) ? $repository['branch'] : 'master';
+    $git_branch = isset($repository['branch']) ? "-b " . $repository['branch'] : '';
     $checkout_directory = isset($repository['checkout_dir']) ? $repository['checkout_dir'] : $this->build->getSourceDirectory();
     // TODO: Ensure we don't end up with double slashes
     // Validate target directory.  Must be within workingdir.
@@ -151,46 +108,22 @@ class Checkout extends BuildTaskBase implements BuildStepInterface, BuildTaskInt
       $this->io->drupalCIError("Directory Error", "The checkout directory <info>$directory</info> is invalid.");
       throw new BuildTaskException("The checkout directory $directory is invalid.");
     }
-    if (substr($repository['repo'],0,4) == 'file') {
-      // If the repository is specified as a local file://tmp/project, then we rsync the
-      // project over to avoid re-composering and re-cloning.
-      $exclude_var = isset($repository['DCI_EXCLUDE']) ? '--exclude="' . $repository['DCI_EXCLUDE'] . '"' : "";
-      $source_dir = substr($repository['repo'],7);
-      $cmd = "rsync -a $exclude_var  $source_dir/. $directory";
-      $this->io->writeln("<comment>Performing rsync of git checkout of $repo $git_branch branch to $directory.</comment>");
-      $this->io->writeln("Rsync Command: $cmd");
-      $this->exec($cmd, $cmdoutput, $result);
 
-      if ($result !== 0) {
-        // rsync threw an error.
-        $this->io->drupalCIError("Checkout Error", "The rsync returned an error.  Error Code: $result");
-        throw new BuildTaskException("The rsync returned an error.  Error Code: $result");
-      }
-
-      $cmd = "cd $directory; git clean -fx; git checkout -f $git_branch";
-      $this->exec($cmd, $cmdoutput, $result);
-
-      if ($result !== 0) {
-        // Git threw an error.
-        $this->io->drupalCIError("Checkout Error", "Unable to change branch.  Error Code: $result");
-        throw new BuildTaskException("Unable to change branch.  Error Code: $result");
-      }
-    } else {
-      $this->io->writeln("<comment>Performing git checkout of $repo $git_branch branch to $directory.</comment>");
-      // TODO: Make sure target directory is empty
-      $git_depth = '';
-      if (isset($repository['depth']) && empty($repository['commit_hash'])) {
-        $git_depth = '--depth ' . $repository['depth'];
-      }
-      $cmd = "git clone -b $git_branch $git_depth $repo '$directory'";
-      $this->io->writeln("Git Command: $cmd");
-      $this->exec($cmd, $cmdoutput, $result);
-
-      if ($result !== 0) {
-        // Git threw an error.
-        $this->io->drupalCIError("Checkout Error", "The git checkout returned an error.  Error Code: $result");
-      }
+    $this->io->writeln("<comment>Performing git checkout of $repo $git_branch to $directory.</comment>");
+    // TODO: Make sure target directory is empty
+    $git_depth = '';
+    if (isset($repository['depth']) && empty($repository['commit_hash'])) {
+      $git_depth = '--depth ' . $repository['depth'];
     }
+    $cmd = "git clone $git_branch $git_depth $repo '$directory'";
+    $this->io->writeln("Git Command: $cmd");
+    $this->exec($cmd, $cmdoutput, $result);
+
+    if ($result !== 0) {
+      // Git threw an error.
+      $this->io->drupalCIError("Checkout Error", "The git checkout returned an error.  Error Code: $result");
+    }
+
 
     if (!empty($repository['commit_hash'])) {
       $cmd =  "cd " . $directory . " && git reset -q --hard " . $repository['commit_hash'] . " ";
