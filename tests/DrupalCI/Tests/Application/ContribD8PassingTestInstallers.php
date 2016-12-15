50e07ac56647bad1b0b010087ba2a93676415f3d
Ryan Aslett <ryan+ddoto@association.drupal.org>
2016-12-15 15:30:41 -0800

    Issue #2597778: adds a test that verifies that everything still works after adding custom installers to composer.json in core

---
+++ tests/DrupalCI/Tests/Application/ContribD8PassingTestInstallers.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Test what happens when a D8.1.x Contrib module has dependencies.
+ * https://dispatcher.drupalci.org/job/default/63496/
+ *
+ * NOTE: This test assumes you have followed the setup instructions in TESTING.md
+ *
+ * @group Application
+ *
+ * @see TESTING.md
+ */
+class ContribD8PassingTest extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dciConfig = [
+    'DCI_AdditionalRepositories=git,git://git.drupal.org/project/block_field.git,8.x-1.x,modules/block_field,1;',
+    'DCI_ComposerInstall=true',
+    'DCI_UseLocalCodebase=/var/lib/drupalci/drupal-checkout',
+    'DCI_LocalBranch=8.3.x',
+    'DCI_DBType=sqlite',
+    'DCI_DBVersion=3.8',
+    'DCI_LocalCommitHash=469d128',
+    'DCI_JobType=simpletest',
+    'DCI_PHPVersion=7',
+    'DCI_TestItem=directory:modules/block_field',
+  ];
+
+  public function testD8Contrib() {
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+    ], $options);
+    $this->assertRegExp('/.*Drupal\\\\block_field\\\\Tests.*/', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
+  }
+}
