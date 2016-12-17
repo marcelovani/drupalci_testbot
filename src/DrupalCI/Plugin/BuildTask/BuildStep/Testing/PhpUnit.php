ab44ce9ba786385588e88ac940449739f6c6c07e
Paul Mitchum <paul@mile23.com>
2016-12-17 01:16:06 +0000

    VERY P-O-C phpunit.yml test definition. You can run ./drupalci run phpunit. See TestbotPassingTest.

--- src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/PhpUnit.php
+++ src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/PhpUnit.php
@@ -12,6 +12,16 @@ use DrupalCI\Plugin\BuildTask\BuildTaskInterface;
 use Pimple\Container;
 
 /**
+ * Test-runner plugin for PHPUnit.
+ *
+ * This plugin is optimized for dogfood purposes. That is, it's being developed
+ * so that we can use the testbot for testing the testbot. It may not be
+ * appropriate for your project.
+ *
+ * Assumptions:
+ * - We can just run phpunit at the root of the project directory without any
+ *   special configuration needs for the project itself.
+ *
  * @PluginID("phpunit")
  */
 class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInterface, Injectable  {
@@ -32,7 +42,7 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
   /* @var \DrupalCI\Build\Codebase\CodebaseInterface */
   protected $codebase;
 
-  protected $runscript = '/core/scripts/run-tests.sh';
+  protected $runscript = '/bin/phpunit';
 
   public function inject(Container $container) {
     parent::inject($container);
@@ -43,64 +53,30 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
   }
 
   /**
-   * @inheritDoc
+   * {@inheritDoc}
    */
   public function configure() {
     // Override any Environment Variables
     if (isset($_ENV['DCI_PHPInterpreter'])) {
       $this->configuration['php'] = $_ENV['DCI_PHPInterpreter'];
     }
-    if (isset($_ENV['DCI_Concurrency'])) {
-      $this->configuration['concurrency'] = $_ENV['DCI_Concurrency'];
-    }
-    if (isset($_ENV['DCI_RTTypes'])) {
-      $this->configuration['types'] = $_ENV['DCI_RTTypes'];
-    }
-    if (isset($_ENV['DCI_RTUrl'])) {
-      $this->configuration['types'] = $_ENV['DCI_RTUrl'];
-    }
-    if (isset($_ENV['DCI_RTColor'])) {
-      $this->configuration['color'] = $_ENV['DCI_RTColor'];
-    }
-    if (isset($_ENV['DCI_TestItem'])) {
-      $this->configuration['testgroups'] = $this->parseTestItems($_ENV['DCI_TestItem']);
-    }
-    if (isset($_ENV['DCI_RTDieOnFail'])) {
-      $this->configuration['die-on-fail'] = $_ENV['DCI_RTDieOnFail'];
-    }
-    if (isset($_ENV['DCI_RTKeepResults'])) {
-      $this->configuration['keep-results'] = $_ENV['DCI_RTKeepResults'];
-    }
-    if (isset($_ENV['DCI_RTKeepResultsTable'])) {
-      $this->configuration['keep-results-table'] = $_ENV['DCI_RTKeepResultsTable'];
-    }
-    if (isset($_ENV['DCI_RTVerbose'])) {
-      $this->configuration['verbose'] = $_ENV['DCI_RTVerbose'];
-    }
-
   }
 
   /**
    * @inheritDoc
    */
   public function run() {
-
-    $this->setupSimpletestDB($this->build);
-    $status = $this->generateTestGroups();
-    if ($status > 0) {
-      return $status;
-    }
+    // Build a phpunit command.
     $command = ["cd " . $this->environment->getExecContainerSourceDir() . " && sudo -u www-data php " . $this->environment->getExecContainerSourceDir() . $this->runscript];
-    $this->configuration['dburl'] = $this->system_database->getUrl();
-    $command[] = $this->getRunTestsFlagValues($this->configuration);
-    $command[] = $this->getRunTestsValues($this->configuration);
+//    $this->configuration['dburl'] = $this->system_database->getUrl();
+    $command[] = $this->getPhpUnitFlagValues($this->configuration);
+    $command[] = $this->getPhpUnitValues($this->configuration);
+    // Special case for log-junit, since it's the file name rather than the
+    // whole path.
+/*    if (isset($this->configuration['log-junit'])) {
+      $command['log-junit'] = $this->build->getXmlDirectory() . "/" . $this->pluginLabel . "testresults.xml";
+    }*/
 
-
-    if (isset($this->configuration['extension_test']) && ($this->configuration['extension_test'])) {
-      $command[] = "--directory " . $this->codebase->getTrueModuleDirectory('modules');
-    } else {
-      $command[] = $this->configuration['testgroups'];
-    }
     $command_line = implode(' ', $command);
 
     $result = $this->environment->executeCommands($command_line);
@@ -109,22 +85,19 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
     if (strpos($result->getOutput(), 'ERROR: No valid tests were specified.') !== FALSE){
       $result->setSignal(0);
     }
-    // Last thing. JunitFormat the output.
-    $this->generateJunitXml();
 
     //Save some artifacts for the build
     $this->build->addContainerArtifact("/var/log/apache2/error.log");
     $this->build->addContainerArtifact("/var/log/supervisor/phantomjs.err.log");
     $this->build->addContainerArtifact("/var/log/supervisor/phantomjs.out.log");
-    $this->build->addContainerArtifact($this->environment->getExecContainerSourceDir() . '/sites/default/files/simpletest');
 
     $label = '';
     if (isset($this->pluginLabel)) {
       $label = $this->pluginLabel . ".";
     }
 
-    $this->build->saveStringArtifact($label . 'simpletestoutput.txt', $result->getOutput());
-    $this->build->saveStringArtifact($label . 'simpletesterror.txt', $result->getError());
+    $this->build->saveStringArtifact($label . 'phpunitoutput.txt', $result->getOutput());
+    $this->build->saveStringArtifact($label . 'phpuniterror.txt', $result->getError());
 
     return $result->getSignal();
   }
@@ -134,18 +107,11 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
    */
   public function getDefaultConfiguration() {
     return [
-      'testgroups' => '--all',
-      'concurrency' => 4,
-      'types' => 'Simpletest,PHPUnit-Unit,PHPUnit-Kernel,PHPUnit-Functional',
-      'url' => 'http://localhost/checkout',
       'php' => '/opt/phpenv/shims/php',
       'color' => TRUE,
-      'die-on-fail' => FALSE,
-      'keep-results' => TRUE,
-      'keep-results-table' => FALSE,
-      'verbose' => FALSE,
-      // testing modules or themes?
-      'extension_test' => FALSE,
+      'stop-on-error' => TRUE,
+      'stop-on-failure' => FALSE,
+      'log-junit' => $this->pluginLabel . '.testresults.xml',
     ];
   }
 
@@ -225,14 +191,12 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
    * @return string
    *   The assembled command line fragment.
    */
-  protected function getRunTestsFlagValues($config) {
+  protected function getPhpUnitFlagValues($config) {
     $command = [];
     $flags = [
       'color',
-      'die-on-fail',
-      'keep-results',
-      'keep-results-table',
-      'verbose',
+      'stop-on-error',
+      'stop-on-failure',
     ];
     foreach($config as $key => $value) {
       if (in_array($key, $flags)) {
@@ -253,16 +217,9 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
    * @return string
    *   The assembled command line fragment.
    */
-  protected function getRunTestsValues($config) {
+  protected function getPhpUnitValues($config) {
     $command = [];
     $args = [
-      'concurrency',
-      'dburl',
-      'sqlite',
-      'types',
-      'url',
-      'xml',
-      'php',
     ];
     foreach ($config as $key => $value) {
       if (in_array($key, $args)) {
@@ -273,201 +230,5 @@ class PhpUnit extends BuildTaskBase implements BuildStepInterface, BuildTaskInte
     }
     return implode(' ', $command);
   }
-  /**
-   * {@inheritdoc}
-   */
-  public function generateJunitXml() {
-
-    // Load the list of tests from the testgroups.txt build artifact
-    // This gets generated in the containers, into the container artifact dir
-    $test_listfile = $this->build->getArtifactDirectory() . '/testgroups.txt';
-    $test_list = file($test_listfile, FILE_IGNORE_NEW_LINES);
-    $test_list = array_slice($test_list, 4);
-
-    $test_groups = $this->parseGroups($test_list);
-
-    // @TODO fix this api. This seems a little obtuse.
-    $db = $this->results_database->connect($this->results_database->getDbname());
-
-    // query for simpletest results
-    $results_map = array(
-      'pass' => 'Pass',
-      'fail' => 'Fail',
-      'exception' => 'Exception',
-      'debug' => 'Debug',
-    );
-
-    $q_result = $db->query('SELECT * FROM simpletest ORDER BY test_id, test_class, message_id;');
-
-    $classes = [];
-
-    while ($result = $q_result->fetch(\PDO::FETCH_ASSOC)) {
-      if (isset($results_map[$result['status']])) {
-        // Set the group from the lookup table
-        $test_group = $test_groups[$result['test_class']];
-
-        // Set the test class
-        if (isset($result['test_class'])) {
-          $test_class = $result['test_class'];
-        }
-        // Jenkins likes to see the groups and classnames together. -
-        // This might need to be re-addressed when we look at the tests.
-        $classname = $test_groups[$test_class] . '.' . $test_class;
-
-        // Cleanup the class, and the parens from the test method name
-        $test_method = substr($result['function'], strpos($result['function'], '>') + 1);
-        $test_method = substr($test_method, 0, strlen($test_method) - 2);
-
-        //$classes[$test_group][$test_class][$test_method]['classname'] = $classname;
-        $length = strlen($this->environment->getExecContainerSourceDir());
-        $result['file'] = substr($result['file'],$length+1); // Trim off source dir
-        $classes[$test_group][$test_class][$test_method][] = array(
-          'status' => $result['status'],
-          'type' => $result['message_group'],
-          'message' => strip_tags(htmlspecialchars_decode($result['message'],ENT_QUOTES)),
-          'line' => $result['line'],
-          'file' => $result['file'],
-        );
-      }
-    }
-    $this->_build_xml($classes);
-  }
-
-  private function _build_xml($test_result_data) {
-    // Maps statuses to their xml element for each testcase.
-    $element_map = array(
-      'pass' => 'system-out',
-      'fail' => 'failure',
-      'exception' => 'error',
-      'debug' => 'system-err',
-    );
-    // Create an xml file per group?
-
-    $test_group_id = 0;
-    $doc = new \DOMDocument('1.0');
-    $test_suites = $doc->createElement('testsuites');
-
-    // TODO: get test name data from the build.
-    $test_suites->setAttribute('name', "TODO SET");
-    $test_suites->setAttribute('time', "TODO SET");
-    $total_failures = 0;
-    $total_tests = 0;
-    $total_exceptions = 0;
-
-    // Go through the groups, and create a testsuite for each.
-    foreach ($test_result_data as $groupname => $group_classes) {
-      $group_failures = 0;
-      $group_tests = 0;
-      $group_exceptions = 0;
-      $test_suite = $doc->createElement('testsuite');
-      $test_suite->setAttribute('id', $test_group_id);
-      $test_suite->setAttribute('name', $groupname);
-      // While more pure, we should probably inject a date/time service.
-      // For now we do not need the timestamp on group data.
-      //  $test_suite->setAttribute('timestamp', date('c'));
-      $test_suite->setAttribute('hostname', "TODO: Set Hostname");
-      $test_suite->setAttribute('package', $groupname);
-      // TODO: time test runs. $test_group->setAttribute('time', $test_group_id);
-      // TODO: add in the properties of the build into the test run.
-
-      // Loop through the classes in each group
-      foreach ($group_classes as $class_name => $class_methods) {
-        foreach ($class_methods as $test_method => $method_results) {
-          $test_case = $doc->createElement('testcase');
-          $test_case->setAttribute('classname', $groupname . "." . $class_name);
-          $test_case->setAttribute('name', $test_method);
-          $test_case_status = 'pass';
-          $test_case_assertions = 0;
-          $test_case_exceptions = 0;
-          $test_case_failures = 0;
-          $test_output = '';
-          $fail_output = '';
-          $exception_output = '';
-          foreach ($method_results as $assertion) {
-            $assertion_result = $assertion['status'] . ": [" . $assertion['type'] . "] Line " . $assertion['line'] . " of " . $assertion['file'] . ":\n" . $assertion['message'] . "\n\n";
-            $assertion_result = preg_replace('/[^\x{0009}\x{000A}\x{000D}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}\x{10000}-\x{10FFFF}]/u', '�', $assertion_result);
-
-            // Keep track of overall assersions counts
-            if (!isset($assertion_counter[$assertion['status']])) {
-              $assertion_counter[$assertion['status']] = 0;
-            }
-            $assertion_counter[$assertion['status']]++;
-            if ($assertion['status'] == 'exception') {
-              $test_case_exceptions++;
-              $group_exceptions++;
-              $total_exceptions++;
-              $test_case_status = 'failed';
-              $exception_output .= $assertion_result;
-            } else if ($assertion['status'] == 'fail'){
-              $test_case_failures++;
-              $group_failures++;
-              $total_failures++;
-              $test_case_status = 'failed';
-              $fail_output .= $assertion_result;
-            }
-            elseif (($assertion['status'] == 'debug')) {
-              $test_output .= $assertion_result;
-            }
-
-            $test_case_assertions++;
-            $group_tests++;
-            $total_tests++;
-
-          }
-          if ($test_case_failures > 0) {
-            $element = $doc->createElement("failure");
-            $element->setAttribute('message', $fail_output);
-            $element->setAttribute('type', "fail");
-            $test_case->appendChild($element);
-          }
-
-          if ($test_case_exceptions > 0 ) {
-            $element = $doc->createElement("error");
-            $element->setAttribute('message', $exception_output);
-            $element->setAttribute('type', "exception");
-            $test_case->appendChild($element);
-          }
-          $std_out = $doc->createElement('system-out');
-          $output = $doc->createCDATASection($test_output);
-          $std_out->appendChild($output);
-          $test_case->appendChild($std_out);
-
-          // TODO: Errors and Failures need to be set per test Case.
-          $test_case->setAttribute('status', $test_case_status);
-          $test_case->setAttribute('assertions', $test_case_assertions);
-          // $test_case->setAttribute('time', "TODO: track time");
-
-          $test_suite->appendChild($test_case);
-
-        }
-      }
-
-      // Should this count the tests as part of the loop, or just array_count?
-      $test_suite->setAttribute('tests', $group_tests);
-      $test_suite->setAttribute('failures', $group_failures);
-      $test_suite->setAttribute('errors', $group_exceptions);
-      /* TODO: Someday simpletest will disable or skip tests based on environment
-      $test_group->setAttribute('disabled', $test_group_id);
-      $test_group->setAttribute('skipped', $test_group_id);
-      */
-      $test_suites->appendChild($test_suite);
-      $test_group_id++;
-    }
-    $test_suites->setAttribute('tests', $total_tests);
-    $test_suites->setAttribute('failures', $total_failures);
-    // $test_suites->setAttribute('disabled', "TODO SET");
-    $test_suites->setAttribute('errors', $total_exceptions);
-    $doc->appendChild($test_suites);
-    $label = '';
-    if (isset($this->pluginLabel)){
-      $label = $this->pluginLabel . ".";
-    }
-
-    $xml_output_file = $this->build->getXmlDirectory() . "/" . $label . "testresults.xml";
-    file_put_contents($xml_output_file, $doc->saveXML());
-    $this->io->writeln("<info>Reformatted test results written to <options=bold>" . $xml_output_file . '</options=bold></info>');
-    $this->build->addArtifact($xml_output_file);
-  }
-
 
 }
---
+++ tests/DrupalCI/Tests/Application/TestbotPassingTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Test trying to run the testbot's tests under the testbot.
+ *
+ * Test what happens when a D8.1.x Contrib module has dependencies. *
+ * NOTE: This test assumes you have followed the setup instructions in TESTING.md
+ *
+ * @group Application
+ *
+ * @see TESTING.md
+ */
+class TestbotPassingTest extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $dciConfig = [
+    'DCI_DogfoodRepository=https://git.drupal.org/project/drupalci_testbot.git',
+    'DCI_DogfoodBranch=dev',
+    'DCI_JobType=phpunit',
+    'DCI_PHPVersion=7',
+  ];
+
+  public function testTestbot() {
+    $this->markTestIncomplete("This test fails because the testbot's test fails, probably because it has so many dependencies.");
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+    ], $options);
+    $this->assertRegExp('/CHANGE THIS WHEN THINGS MIGHT PASS/', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
+  }
+}
