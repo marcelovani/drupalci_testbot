2638674c848920b9720bee06ec38c1d74772e7f2
Paul Mitchum <paul@mile23.com>
2016-12-30 16:41:49 -0800

    Issue #2837110: Remove DCI_PHPInterpreter environmental variable.

--- src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/Simpletest.php
+++ src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/Simpletest.php
@@ -47,9 +47,6 @@ class Simpletest extends BuildTaskBase implements BuildStepInterface, BuildTaskI
    */
   public function configure() {
     // Override any Environment Variables
-    if (isset($_ENV['DCI_PHPInterpreter'])) {
-      $this->configuration['php'] = $_ENV['DCI_PHPInterpreter'];
-    }
     if (isset($_ENV['DCI_Concurrency'])) {
       $this->configuration['concurrency'] = $_ENV['DCI_Concurrency'];
     }
@@ -188,7 +185,7 @@ class Simpletest extends BuildTaskBase implements BuildStepInterface, BuildTaskI
 
   protected function generateTestGroups() {
     $testgroups_file = $this->environment->getContainerArtifactDir() . "/testgroups.txt";
-    $cmd = "sudo -u www-data php " . $this->environment->getExecContainerSourceDir() . $this->runscript . " --list --php " . $this->configuration['php'] . " > " . $testgroups_file;
+    $cmd = "sudo -u www-data php " . $this->environment->getExecContainerSourceDir() . $this->runscript . " --list > " . $testgroups_file;
     $result = $this->environment->executeCommands($cmd);
     $host_testgroups = $this->build->getArtifactDirectory() . '/testgroups.txt';
     $this->build->addContainerArtifact($testgroups_file);
