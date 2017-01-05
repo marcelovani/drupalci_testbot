9a456876de03b487280ddc26c6c050d5f897bac2
Ryan Aslett <ryan+ddoto@association.drupal.org>
2017-01-05 15:59:10 -0800

    Issue #2841478: adds a test to verify that a new composer.json will result in dependency recalculation

---
+++ tests/DrupalCI/Tests/Application/CodebaseAssemble/NewComposerJsonModifiedFilesTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Test what happens when a D8.1.x Contrib module has dependencies.
+ * https://dispatcher.drupalci.org/job/default/63496/
+ *
+ * NOTE: This test assumes you have followed the setup instructions in TESTING.md
+ *
+ * @group Application
+ *
+ * @see TESTING.md
+ */
+class NewComposerJsonModifiedFilesTest extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function testD8Contrib() {
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+      'definition' => 'tests/DrupalCI/Tests/Application/Fixtures/build.NewComposerJsonModifiedFiles.yml',
+    ], $options);
+    $display = $app_tester->getDisplay();
+    $this->assertRegExp('/.*composer.json changed by patch: recalculating depenendices.*/', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
+  }
+}
