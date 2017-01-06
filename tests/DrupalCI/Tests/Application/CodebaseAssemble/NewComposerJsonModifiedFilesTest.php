55bcf7fc714ecc863d88becdbc2cb50571abed8d
Paul Mitchum <paul@mile23.com>
2017-01-05 16:47:44 -0800

    Issue #2841478: Added a test of the modified files that the Codebase object found.

--- tests/DrupalCI/Tests/Application/CodebaseAssemble/NewComposerJsonModifiedFilesTest.php
+++ tests/DrupalCI/Tests/Application/CodebaseAssemble/NewComposerJsonModifiedFilesTest.php
@@ -29,6 +29,15 @@ class NewComposerJsonModifiedFilesTest extends DrupalCIFunctionalTestBase {
       'command' => 'run',
       'definition' => 'tests/DrupalCI/Tests/Application/Fixtures/build.NewComposerJsonModifiedFiles.yml',
     ], $options);
+    // This test applies a patch which adds a composer.json file to the contrib
+    // module under test. Verify that codebase counts the added file as having
+    // been modified.
+    $codebase = $this->getContainer()['codebase'];
+    $this->assertContains(
+      $codebase->getTrueExtensionDirectory('modules') . '/composer.json',
+      $codebase->getModifiedFiles()
+    );
+    // Verify the output.
     $display = $app_tester->getDisplay();
     $this->assertRegExp('/.*composer.json changed by patch: recalculating depenendices.*/', $app_tester->getDisplay());
     $this->assertEquals(0, $app_tester->getStatusCode());
