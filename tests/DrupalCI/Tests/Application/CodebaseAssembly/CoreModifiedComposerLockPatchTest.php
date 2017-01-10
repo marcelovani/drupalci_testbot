b5aaa4d679e4500a86fbfcdd6694d334c8734432
Ryan Aslett <ryan+ddoto@association.drupal.org>
2017-01-10 15:31:50 -0800

    Issue #2842529: Adds a test to prove that core cannot, currently, patch composer.lock and have it work

---
+++ tests/DrupalCI/Tests/Application/CodebaseAssembly/CoreModifiedComposerLockPatchTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace DrupalCI\Tests\Application\CodebaseAssembly;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Basic test that proves that drupalci can execute a simpletest and generate a result
+ *
+ * NOTE: This test assumes you have followed the setup instructions in TESTING.md
+ *
+ * @group Application
+ * @group docker
+ *
+ * @see TESTING.md
+ */
+class CoreModifiedComposerLockPatchTest extends DrupalCIFunctionalTestBase {
+
+
+  public function testBasicTest() {
+    $app = $this->getConsoleApp();
+    $options = ['interactive' => FALSE];
+    $app_tester = new ApplicationTester($app);
+    $app_tester->run([
+      'command' => 'run',
+      'definition' => 'tests/DrupalCI/Tests/Application/Fixtures/build.CoreModifiedComposerLockPatchTest.yml',
+    ], $options);
+    $build = $this->getCommand('run')->getBuild();
+
+    $codebase = $this->getContainer()['codebase'];
+    $this->assertContains('composer.lock',
+      $codebase->getModifiedFiles()
+    );
+
+
+    // TODO: [0] is very brittle. We need to make build artifacts have labels.
+    // See: https://www.drupal.org/node/2842547
+
+    $build_artifacts = $build->getBuildArtifacts();
+    $installed_json = json_decode(file_get_contents($build_artifacts[0]->getArtifactPath()), TRUE);
+    foreach ($installed_json as $package) {
+      if ($package['name'] == "symfony/class-loader") {
+        $this->assertEquals('v2.8.15', $package['version']);
+      }
+    }
+
+    $this->assertRegExp('/Adding testing \(require-dev\) dependencies./', $app_tester->getDisplay());
+    $this->assertEquals(0, $app_tester->getStatusCode());
+  }
+
+}
