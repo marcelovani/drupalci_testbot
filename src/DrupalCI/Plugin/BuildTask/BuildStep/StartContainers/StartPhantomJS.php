0f8d63a30baf3a1f5273280f716f8e95e5f852f0
Ryan Aslett <ryan+ddoto@association.drupal.org>
2017-02-11 16:42:31 -0800

    Issue #2843673: Renames prepare filesystem to start_phantomjs

--- src/DrupalCI/Plugin/BuildTask/BuildStep/StartContainers/StartPhantomJS.php
+++ src/DrupalCI/Plugin/BuildTask/BuildStep/StartContainers/StartPhantomJS.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DrupalCI\Plugin\BuildTask\BuildStep\Filesystem;
+namespace DrupalCI\Plugin\BuildTask\BuildStep\StartContainers;
 
 use DrupalCI\Injectable;
 use DrupalCI\Plugin\BuildTask\BuildStep\BuildStepInterface;
@@ -13,9 +13,9 @@ use Pimple\Container;
  *
  * @todo: Rename this plugin, refactor to other places.
  *
- * @PluginID("prepare_filesystem")
+ * @PluginID("start_phantomjs")
  */
-class PrepareFilesystem extends BuildTaskBase implements BuildStepInterface, BuildTaskInterface, Injectable {
+class StartPhantomJS extends BuildTaskBase implements BuildStepInterface, BuildTaskInterface, Injectable {
 
   /* @var  \DrupalCI\Build\Environment\EnvironmentInterface */
   protected $environment;
@@ -32,16 +32,14 @@ class PrepareFilesystem extends BuildTaskBase implements BuildStepInterface, Bui
     $sourcedir = $this->environment->getExecContainerSourceDir();
     $setup_commands = [
       'supervisorctl start phantomjs',
-      'php -v',
     ];
     $result = $this->environment->executeCommands($setup_commands);
-    //phantomjs still fails on
-    // 5.3 /5.4 so leave this out for now.
-    //    if ($result !== 0)
-    // Directory setup failed threw an error.
-    //      $this->terminateBuild("Prepare Filesystem failed", "Setting up the filesystem failed:  Error Code: $result");
-    //    }
-    //return $result->getSignal();
+    $return = $result->getSignal();
+    if ($return !== 0) {
+      // Directory setup failed threw an error.
+      $this->terminateBuild("Unable to start phantomjs", $result->getError());
+    }
+    return $result->getSignal();
   }
 
 }
