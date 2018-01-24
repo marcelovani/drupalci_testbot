b8d1952d16badca4ed334b855592898701e8c33b
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2018-01-23 17:49:31 -0800

    Need the filesystem prep after all if we dont do that in the simpletest plugin. Probably needs to go somewhere else

--- src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/NightwatchJS.php
+++ src/DrupalCI/Plugin/BuildTask/BuildStep/Testing/NightwatchJS.php
@@ -6,9 +6,9 @@ use DrupalCI\Plugin\BuildTask\BuildStep\BuildStepInterface;
 use DrupalCI\Plugin\BuildTaskBase;
 use Pimple\Container;
 /**
- * @PluginID("javascript")
+ * @PluginID("nightwatchjs")
  */
-class Javascript extends BuildTaskBase implements BuildStepInterface {
+class NightwatchJS extends BuildTaskBase implements BuildStepInterface {
   /* @var  \DrupalCI\Build\Environment\DatabaseInterface */
   protected $system_database;
   /**
@@ -24,7 +24,7 @@ class Javascript extends BuildTaskBase implements BuildStepInterface {
    *
    * @var string
    */
-  protected $runscript = 'NODE_ENV=testbot yarn --cwd /var/www/html/core test:js';
+  protected $runscript = 'yarn --cwd /var/www/html/core test:js';
   /**
    * Junit XML builder service.
    *
@@ -36,12 +36,13 @@ class Javascript extends BuildTaskBase implements BuildStepInterface {
     $this->system_database = $container['db.system'];
     $this->environment = $container['environment'];
     $this->codebase = $container['codebase'];
-    $this->junitXmlBuilder = $container['junit_xml_builder'];
   }
   /**
    * @inheritDoc
    */
   public function run() {
+
+    $this->prepareFilesystem();
 
     $hostname = $this->environment->getChromeContainerHostname();
     $database_url = $this->system_database->getUrl();
@@ -50,7 +51,7 @@ class Javascript extends BuildTaskBase implements BuildStepInterface {
     $cmd = "cp {$this->codebase->getSourceDirectory()}/core/nightwatch.settings.json.default {$this->codebase->getSourceDirectory()}/core/nightwatch.settings.json";
     $this->exec($cmd, $output, $return);
 
-    $runscript = "BASE_URL={$base_url} DB_URL={$database_url} WEBDRIVER_HOSTNAME={$hostname} NIGHTWATCH_OUTPUT={$this->codebase->getSourceDirectory()}/nightwatch_output {$this->runscript}";
+    $runscript = "BASE_URL={$base_url} DB_URL={$database_url} WEBDRIVER_HOSTNAME={$hostname} NIGHTWATCH_OUTPUT={$this->environment->getExecContainerSourceDir()}/nightwatch_output NODE_ENV=testbot {$this->runscript}";
     $result = $this->environment->executeCommands("$runscript");
 
     // Save some artifacts for the build
@@ -58,6 +59,24 @@ class Javascript extends BuildTaskBase implements BuildStepInterface {
       $this->saveContainerArtifact($this->environment->getExecContainerSourceDir() . '/nightwatch_output', 'nightwatch_xml');
     }
     return 0;
+  }
+
+  /**
+   * Prepare the filesystem for a run-tests.sh run.
+   *
+   */
+  protected function prepareFilesystem() {
+    $sourcedir = $this->environment->getExecContainerSourceDir();
+    $setup_commands = [
+      'ln -s ' . $sourcedir . ' ' . $sourcedir . '/subdirectory',
+      'chown -fR www-data:www-data ' . $sourcedir . '/sites',
+    ];
+    $result = $this->environment->executeCommands($setup_commands);
+    $return = $result->getSignal();
+    if ($return !== 0) {
+      // Directory setup failed threw an error.
+      $this->terminateBuild("Prepare filesystem failed", "Setting up the filesystem failed:  Error Code: $return");
+    }
   }
 
 }
