48a888edade2d71283b363a0ce15ea5780586681
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2018-01-25 14:39:17 -0800

    More unused cruft

--- src/DrupalCI/Console/Command/Drupal/DrupalCICommandBase.php
+++ src/DrupalCI/Console/Command/Drupal/DrupalCICommandBase.php
@@ -38,17 +38,4 @@ class DrupalCICommandBase extends Command {
     $this->io = $this->container['console.io'];
   }
 
-  protected function showArguments(InputInterface $input, OutputInterface $output) {
-    $output->writeln('<info>Arguments:</info>');
-    $items = $input->getArguments();
-    foreach ($items as $name => $value) {
-      $output->writeln(' ' . $name . ': ' . print_r($value, TRUE));
-    }
-    $output->writeln('<info>Options:</info>');
-    $items = $input->getOptions();
-    foreach ($items as $name => $value) {
-      $output->writeln(' ' . $name . ': ' . print_r($value, TRUE));
-    }
-  }
-
 }
