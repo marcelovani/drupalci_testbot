3c6b47f7517a0531bee50163efbf6822da63eaba
Ryan Aslett (Mixologic) <ryan@association.drupal.org>
2018-04-01 07:47:11 -0700

    prelim test of local patches and coredumps

---
+++ tests/DrupalCI/Tests/Application/CoreSegfaultTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace DrupalCI\Tests\Application;
+
+use DrupalCI\Tests\DrupalCIFunctionalTestBase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+
+/**
+ * Basic test that proves that drupalci can execute a simpletest and generate a result
+ *
+ * NOTE: This test assumes you have followed the setup instructions in TESTING.md
+ *
+ * @group Application
+ * @group docker
+ * @group Xml
+ *
+ * @see TESTING.md
+ */
+class CoreSegfaultTest extends DrupalCIFunctionalTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testSegfaultTest() {
+
+    $options = ['interactive' => FALSE];
+    $this->app_tester->run([
+      'command' => 'run',
+      'definition' => 'tests/DrupalCI/Tests/Application/Fixtures/build.CoreSegfaultTest.yml',
+    ], $options);
+    /* @var $build \DrupalCI\Build\BuildInterface */
+    $build = $this->getCommand('run')->getBuild();
+    $display = $this->app_tester->getDisplay();
+    $this->assertNotRegExp('/.*legacydevelopment*/', $this->app_tester->getDisplay());
+    $this->assertRegExp('/.*Drupal\\\\KernelTests\\\\Core\\\\Routing\\\\UrlIntegrationTest*/', $this->app_tester->getDisplay());
+    // Look for junit xml results file
+    $output_file = $build->getXmlDirectory() . "/standard.testresults.xml";
+    $this->assertFileExists($output_file);
+    // create a test fixture that contains the xml output results.
+    $this->assertXmlFileEqualsXmlFile(__DIR__ . '/Fixtures/CoreD8PassingTest_testresults.xml', $output_file);
+    $this->assertEquals(0, $this->app_tester->getStatusCode());
+
+    $this->assertBuildOutputJson($build, 'buildLabel', 'Build Successful');
+    $this->assertBuildOutputJson($build, 'buildDetails', '');
+  }
+
+}
